2112c398e720bf29fd10baf7c9294543513478e4
Gabriele Canepa <gabriele.canepa93@gmail.com>
2019-11-07 22:58:06 +0000

    Initial configuration

---
+++ eslint-local-rules.js
@@ -0,0 +1,28 @@
+// eslint-disable-next-line no-undef
+const moduleVisitor = require('eslint-module-utils/moduleVisitor').default
+
+module.exports = {
+  'no-relative-parent-imports': {
+    create(context) {
+      if (context.getFilename() === '<text>') return {}
+
+      const checkSourceValue = node => {
+        if (!node.value.includes('../')) return
+
+        context.report({
+          message: 'Relative imports from parent directories are not allowed. Use absolute imports instead.',
+          node,
+        })
+      }
+      return moduleVisitor(checkSourceValue, context.options[0])
+    },
+    meta: {
+      docs: {
+        category: 'Possible Errors',
+        description: 'disallow parent relative imports',
+        recommended: false,
+      },
+      schema: [],
+    },
+  },
+}
---
+++ src/app/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const App = () => (
+  <div>
+    <h1>{'Talkdesk Apps Listing'}</h1>
+  </div>
+)
+
+export default App
